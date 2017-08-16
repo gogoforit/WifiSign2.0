ee0703736e87f171e64b2221d0b2583983391397
gogoforit <715157026@qq.com>
2017-08-16 11:59:44 +0800

    fix bug

--- app/moduls/remarks.py
+++ app/moduls/remarks.py
@@ -5,4 +5,5 @@ class Remarks(Document):
     class_id = StringField(required=True)
     remarks = StringField(required=True)
     class_num = StringField(required=True)
+    date = StringField(required=True)
     meta = {'db_alias': 'local_db'}
